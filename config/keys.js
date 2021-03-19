c8b44436daee6dd4141ecbcde709c850cab6b7c0
Gouransh Sachdeva <gouranshsachdeva2000@gmail.com>
2021-03-19 19:01:58 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://gouransh:hellomern@cluster0.f45rh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongoURI: "url",
     secretOrKey: "secret" 
   };
